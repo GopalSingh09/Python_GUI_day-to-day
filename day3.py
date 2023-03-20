515c95594dfcbce9361c51bf738e22df6e0dc1a4
Gopal Singh <109897876+GopalSingh09@users.noreply.github.com>
2023-03-21 01:29:03 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,32 @@
+# day 3
+
+
+from tkinter import *
+
+root = Tk()
+# Chapter 6 : Frames in python
+
+# my_frame = Frame(root, bg = "blue", bd= 6, borderwidth=10 , height=150, width=200, highlightbackground="black",  relief= SUNKEN  )
+# my_frame.pack(side=LEFT, anchor="nw")
+# label1 = Label(my_frame, text="Hi, this is me.")
+# label1.pack(pady=50, padx=50)
+
+
+# chapter 7: buttons
+# def mssg():
+#     bik = Label(text="WHy did you click on me -.-")
+#     bik.pack()
+# # flash()
+# def flash(event):
+#     btn.config(bg = "yellow")
+#     root.after(100, lambda : b.config(bg='Red'))
+# root.bind("KeyPress -f", flash)
+#
+# btn = Button(my_frame, text="Click", bd = 6, command= mssg, fg="white" )
+# btn.config(bg='Red')
+# btn.pack(padx=100, pady=150)
+
+
+
+
+root.mainloop()
